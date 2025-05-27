b13fe68ffc76174c379311f30aa7e6993c21f1ac
Lahav Rabinovitz <130746203+lahav97@users.noreply.github.com>
2025-05-27 21:25:00 +0300

    Remove unnecessary files: yarn.lock, test configs, and build artifacts

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'jest-expo',
-  transformIgnorePatterns: [
-    'node_modules/(?!(expo|expo-asset|expo-constants|expo-font|expo-linear-gradient|expo-haptics|expo-modules-core|expo-auth-session|expo-web-browser|expo-localization|react-native-phone-number-input|react-native-country-picker-modal|@expo|@react-native|react-native|react-native-vector-icons)/)',
-  ],
-};
